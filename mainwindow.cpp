732ab495776aee06fa246fb409aa005edc83841c
Filip Sobolewski <62287972+Cyrus440@users.noreply.github.com>
2022-03-11 20:06:31 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_btn_image_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this,tr("Wybierz "), "", tr("Obrazy (*.png *.jpg *.jpeg"));
+    if (QString::compare(filename, QString()) != 0)
+    {
+        QImage image;
+        bool valid = image.load(filename);
+        if(valid)
+        {
+            image = image.scaledToWidth(ui->lbl_image->width(), Qt::SmoothTransformation);
+            image = image.scaledToHeight(ui->lbl_image->height(), Qt::SmoothTransformation);
+            ui->lbl_image->setPixmap(QPixmap::fromImage(image));
+        }
+        else
+        {
+            // Error handling
+        }
+    }
+}
+
